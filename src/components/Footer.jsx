4ad09f4d6d45fac4ba4f48af6f9a9a576d0ec634
Saurav Satpathy <saurav.satpathy98@gmail.com>
2026-01-13 17:43:16 +0530

    1.Chnages in footer . 2.Added a border in experience 3.Added Company Logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react'
-import facebook from '../assets/facebook-icon.svg'
+import React, { useState } from 'react'
 import twitter from '../assets/twitter-icon.svg'
 import linkedin from '../assets/linkedin-icon.svg'
 import github from '../assets/github-icon.svg'
@@ -11,20 +10,12 @@ function Footer() {
   return (
     <footer id="footer">
       <div className="container">
-        <a href="mailto:communitypro47@gmail.com">communitypro47@gmail.com</a>
+        <a href="mailto:communitypro47@gmail.com">saurav.satpathy98@gmail.com</a>
         <div className="social">
-          <a href="#"><img src={facebook} alt="Facebook" /></a>
-          <a href="#"><img src={twitter} alt="Twitter" /></a>
-          <a href="#"><img src={linkedin} alt="LinkedIn" /></a>
-          <a href="#"><img src={github} alt="GitHub" /></a>
-          <a href="#"><img src={hashnode} alt="Hashnode" /></a>
+          <a href="https://www.linkedin.com/in/saurav-satpathy-612ba21a3"><img src={linkedin} alt="LinkedIn" /></a>
+          <a href="https://github.com/Satapathy-GitWorkshop"><img src={github} alt="GitHub" /></a>
         </div>
-        <div className="attribution">
-          <a href="https://github.com/CommunityPro/portfolio-html" target="_blank">
-            <img src="https://user-images.githubusercontent.com/62628408/157202263-9174da24-b19a-4017-9b7c-a1d26ae8f014.svg" alt="attribution" width="150px" />
-          </a>
-        </div>
-        <p>Copyright &copy; Communitypro {year}, All rights reserved</p>
+        <p>Built with ❤️ by Saurav Satpathy • {year}</p>
       </div>
     </footer>
   )
